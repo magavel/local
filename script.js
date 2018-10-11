f170cfd3cd78163c45f37f5981080cfa908d3b36
celialt <le.troquer.celia@gmail.com>
2018-10-11 16:24:54 +0200

    ajout affichage carte

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+let map, markers = [];
+
+ (function () {
+     initMap();
+ })();
+
 function getCityByPostalCode(postalCode) {
     axios.get(
         apiBasUrl,
@@ -11,12 +17,24 @@ function getCityByPostalCode(postalCode) {
 }
 
 function writesData(response) {
-    let tabbleBody = document.getElementById('dataCityRow');
+    let tabbleBody = document.getElementById('dataCityRow'),
+         mapBounds = [];
     tabbleBody.innerHTML = '';
 
+    clearMarkers();
+
     for (let city of response.data.features) {
+        let lat = city.geometry.coordinates[1],
+             lgn = city.geometry.coordinates[0];
+
         tabbleBody.innerHTML += rowTemplate(city);
+        addMakerInMap(lat, lgn, city.properties.nom);
+
+        mapBounds.push([lat,lgn])
     }
+
+    map.fitBounds(mapBounds);
+    
 }
 
 function rowTemplate(data) {
@@ -29,9 +47,29 @@ function rowTemplate(data) {
     <td>${pro.code}</td>
     <td>${pro.population}</td>
     <td>
-        ${geo.coordinates[0].toFixed(fixedNumber)},
-        ${geo.coordinates[1].toFixed(fixedNumber)}
+        ${geo.coordinates[1].toFixed(fixedNumber)},
+        ${geo.coordinates[0].toFixed(fixedNumber)}
     </td>
 </tr>`;
 }
+
+function initMap() {
+     map = L.map('map').setView([44.562201, 6.079159], 13);
+
+     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+     }).addTo(map);
+ }
+
+ function addMakerInMap(lat, lng, name) {
+     markers.push(
+         L.marker([lat, lng]).addTo(map).bindPopup(name)
+     );
+ }
+
+ function clearMarkers() {
+     for(let marker of markers) {
+         marker.remove();
+     }
+ } 
 
